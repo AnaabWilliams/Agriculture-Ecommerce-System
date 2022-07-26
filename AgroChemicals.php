05534cca78b73aa5d97f910e6e07c127a374aaaf
Williams Abanwai <43754731+AnaabWilliams@users.noreply.github.com>
2022-07-26 02:01:11 +0000

    Add files via upload

---
+++ AgroChemicals.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AgroChemicals extends Model
+{
+    use HasFactory;
+    protected $table ='agro_chemicals';
+}
