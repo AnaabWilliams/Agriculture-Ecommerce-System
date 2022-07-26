16a80da3f29f1a7a1df986cf9185e8b6d7f3d1d7
Williams Abanwai <43754731+AnaabWilliams@users.noreply.github.com>
2022-07-26 01:54:18 +0000

    Add files via upload

---
+++ UserOrders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+
+class UserOrders extends Component
+
+{
+
+    public function render()
+    {
+        $orders = Order::where('user_id', Auth::user()->id)->paginate(12);
+        return view('livewire.user.user-orders',['orders'=>$orders])->layout('layouts.base');
+    }
+}
